da58f273e5c49d7d6fb83b2974c6027b9623ba06
hpareds <pv22014@ues.edu.sv>
2026-02-17 13:17:33 -0600

    Versión funcional con limpieza de pantalla

---
+++ assets/dominio/Factura.py
@@ -0,0 +1,6 @@
+class Factura:
+    def __init__(self, id_factura, fecha, id_cliente, total):
+        self.id_factura = id_factura
+        self.fecha = fecha
+        self.id_cliente = id_cliente
+        self.total = total
---
+++ assets/interfaz/MenuCliente.py
@@ -0,0 +1,45 @@
+from assets.utilidades import limpiar_pantalla
+
+class MenuCliente:
+    def __init__(self, servicio_autenticacion, servicio_producto):
+        self.servicio_autenticacion = servicio_autenticacion
+        self.producto_service = servicio_producto
+
+    def iniciar(self):
+        while True:
+            limpiar_pantalla()
+            print("\n---SISTEMA DE SUPERMERCADO PYTHON---")
+            print("1. Ver productos") 
+            print("2. Mis compras")
+            print("3. Salir")          
+            opcion = input("Seleccione una opción: ")
+
+            if opcion == "1":
+                print("\n---Listado de productos---")
+                self.mostrar_listado()
+
+            elif opcion == "2":
+                print("\n---Historial de compras---")
+            elif opcion == "3":
+                print("\nCerrando sesión cliente...")
+                break
+            else:
+                print("\nOpción inválida. Intente nuevamente.")
+
+    def mostrar_listado(self):
+            print("\n--- LISTADO DE PRODUCTOS ---")
+            # 1. Llamamos al método obtener_todos() del servicio/repo
+            productos = self.producto_service.obtener_todos()
+
+            if not productos:
+                print("No hay productos cargados en el sistema.")
+                return
+
+            # 2. Imprimimos con formato para que se vea ordenado
+            print(f"{'ID':<5} | {'Nombre':<20} | {'Precio':<10} | {'Stock':<5}")
+            print("-" * 50)
+            
+            for p in productos:
+                # Filtro: Solo mostrar si el producto está activo
+                if p.activo == "True":
+                    print(f"{p.id:<5} | {p.nombre:<20} | ${p.precio:<9} | {p.stock:<5}")
